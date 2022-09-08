068b3135d82ce29cd30937e4fe210393ea2591ca
publdaze <78250089+publdaze@users.noreply.github.com>
2022-09-08 23:43:50 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 136 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("");
+
+const solve = () => {
+  const alphabets = Array.from({ length: 26 }, (v, i) =>
+    String.fromCharCode(i + 97)
+  );
+
+  const alphabetIdxs = alphabets.map((alphabet) =>
+    input.findIndex((element) => element == alphabet)
+  );
+
+  console.log(alphabetIdxs.join(" "));
+};
+
+solve();
