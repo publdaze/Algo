572abc9b3b19857b5c0f97c61b025ab28d0e6ff5
publdaze <78250089+publdaze@users.noreply.github.com>
2023-03-18 00:22:23 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,9 +1,11 @@
+// Number.isNaN으로 변경하여 더 엄격하게 검사해줌
 function isNumber(data) {
-  return !isNaN(data);
+  return !Number.isNaN(data);
 }
 
 function solution(str) {
-    const numberList = [...str].filter((char) => isNumber(char)).map((char) => Number(char));
-    
-    return numberList.sort();
+	// map을 먼저 돌려 숫자로 바꾼 후 isNumber 체크
+  const numberList = [...str].map((char) => Number(char)).filter((char) => isNumber(char));
+  
+  return numberList.sort();
 }
