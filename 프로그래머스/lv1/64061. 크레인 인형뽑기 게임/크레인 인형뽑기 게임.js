3b4dc49bd03956d4e504614aa31b3f46898a8833
publdaze <78250089+publdaze@users.noreply.github.com>
2023-03-21 19:18:14 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.65 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -0,0 +1,34 @@
+// i : board : 순서대로 스택이 주어짐, moves : 어느 스택 인형을 옮겼는지
+// o : 터트려진 인형 개수
+// 40m
+
+function solution(board, moves) {
+    let boardObj = {};
+    let basket = [];
+    let bombCnt = 0;
+    
+    board.forEach((line, lineIdx) => {
+        for (const [index, element] of line.entries()) {
+            if (!boardObj[index + 1]) boardObj[index + 1] = [];
+            if (element !== 0) {
+                boardObj[index + 1].unshift(element);
+            }
+        }
+    });
+    
+    moves.forEach((move) => {
+        const top = boardObj[move].pop();
+        
+        if (top) {
+            if(basket.at(-1) === top) {
+                basket.pop();
+                bombCnt += 2;
+            }
+            else {
+                basket.push(top);
+            }
+        }
+    })
+    
+    return bombCnt;
+}
