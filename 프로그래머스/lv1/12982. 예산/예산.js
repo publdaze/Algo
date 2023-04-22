e9b227a749d423ddc4dfde383d906f78fa8f184e
publdaze <78250089+publdaze@users.noreply.github.com>
2023-04-22 20:28:36 +0900

    [level 1] Title: 예산, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.js
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -1,12 +1,11 @@
 function solution(d, budget) {
-    let sum = 0;
     let departmentCnt = 0;
     
     d.sort((a, b) => a - b);
     
-    for (let money of d) {
-        sum += money;
-        if (sum > budget) return departmentCnt;
+    for (let price of d) {
+        budget -= price;
+        if (budget < 0) return departmentCnt;
         departmentCnt += 1;
     }
     
