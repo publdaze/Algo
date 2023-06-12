f5f3cf49316dc439c5a2df3ee9cba1ca7c43057a
publdaze <78250089+publdaze@users.noreply.github.com>
2023-06-12 16:21:31 +0900

    [level 2] Title: 카펫, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -1,7 +1,15 @@
 function solution(brown, yellow) {
+    const cornerCount = 4;
+    
     for (let i = 1; i < yellow; i++) {
-        if (yellow / i * 2 + i * 2 + 4 === brown) {
-            return [yellow / i + 2, i + 2];
+        const yellowWidth = yellow / i;
+        const yellowHeight = i;
+        const brownWidth = yellowWidth + 2;
+        const brownHeight = yellowHeight + 2;
+        const brownCount = brownWidth * 2 + brownHeight * 2 - cornerCount;
+        
+        if (brownCount === brown) {
+            return [brownWidth, brownHeight];
         }
     }
     
