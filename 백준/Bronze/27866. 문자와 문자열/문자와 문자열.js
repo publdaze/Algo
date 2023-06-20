5896ca382fbf687c86bc3888e2d93a9107bbf789
publdaze <78250089+publdaze@users.noreply.github.com>
2023-06-20 15:09:51 +0900

    [Bronze V] Title: 문자와 문자열, Time: 144 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .split("\n");
+
+console.log(input[0][input[1] - 1]);
