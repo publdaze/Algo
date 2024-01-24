6f54cfcf99e6bfda3ce728b1788961e3f2b66e41
publdaze <78250089+publdaze@users.noreply.github.com>
2024-01-24 23:08:42 +0900

    [Bronze II] Title: Hashing, Time: 128 ms, Memory: 9592 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.js
+++ 백준/Bronze/15829. Hashing/Hashing.js
@@ -8,11 +8,14 @@ const input = fs
 const [, str] = input;
 
 let alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+let pow = 1;
 console.log(
   str
     .split("")
     .map((c) => alphabet.indexOf(c) + 1)
-    .reduce((acc, a, i) => {
-      return acc + a * 31 ** i;
+    .reduce((acc, a) => {
+      const calc = acc + ((a * pow) % 1234567891);
+      pow = (pow * 31) % 1234567891;
+      return calc;
     }, 0) % 1234567891
 );
