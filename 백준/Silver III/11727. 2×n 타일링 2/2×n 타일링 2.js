5aa5d21fd8c25a41dc1efc7f39f173b6ef78e479
publdaze <78250089+publdaze@users.noreply.github.com>
2024-02-16 16:05:52 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 196 ms, Memory: 9648 KB -BaekjoonHub

---
+++ 백준/Silver III/11727. 2×n 타일링 2/2×n 타일링 2.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim();
+
+const n = Number(input);
+const dp = [0, 1, 3];
+
+for (let i = 3; i <= n; i++) {
+  dp[i] = (dp[i - 2] * 2 + dp[i - 1]) % 10007;
+}
+
+console.log(dp[n]);
