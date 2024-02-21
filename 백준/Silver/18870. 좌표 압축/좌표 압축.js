d01bbbbb02e2d7fbd4db19b9c1344433b09b9eaa
publdaze <78250089+publdaze@users.noreply.github.com>
2024-02-21 15:38:57 +0900

    [Silver II] Title: 좌표 압축, Time: 2068 ms, Memory: 336776 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.js
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.js
@@ -3,33 +3,19 @@ const input = fs
   .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
   .toString()
   .trim()
-  .split("\n");
+  .split("\n")
+  .slice(1);
 
 // TODO - 본인보다 작은 좌표 개수
 
-const N = Number(input.shift());
 const Xs = input.at(0).split(" ").map(Number);
-const result = Array.from({ length: N }, () => 0);
 
-let pointValues = {};
+let coordinateCompression = {};
 
-const comparePoint = (a, b) => {
-  return pointValues[a] - pointValues[b];
-};
+let uniquePoints = [...new Set(Xs)].sort((a, b) => a - b);
 
-Xs.forEach((X, i) => {
-  pointValues[i] = X;
+uniquePoints.forEach((uniquePoint, i) => {
+  coordinateCompression[uniquePoint] = i;
 });
 
-let i = 0;
-const sortedKeys = Object.keys(pointValues).sort(comparePoint);
-sortedKeys.forEach((key, index) => {
-  if (index === 0) return;
-
-  if (Xs[sortedKeys[index - 1]] < Xs[key]) {
-    i++;
-  }
-  result[key] = i;
-});
-
-console.log(result.join(" "));
+console.log(Xs.map((X) => coordinateCompression[X]).join(" "));
