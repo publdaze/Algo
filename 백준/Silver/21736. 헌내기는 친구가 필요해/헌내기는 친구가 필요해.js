85ab59a17ecd2ff66a55facceb07232f78d8fc29
publdaze <78250089+publdaze@users.noreply.github.com>
2024-02-25 20:50:32 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 408 ms, Memory: 57332 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.js
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.js
@@ -33,6 +33,52 @@ const getDoyeonPoint = () => {
 
 const bfs = (startPoint) => {
   const queue = [startPoint];
+  campus[startPoint[0]][startPoint[1]] = "X";
+
+  let i = 0;
+  while (queue.length > i) {
+    const [raw, col] = queue.at(i++);
+
+    for (let [x, y] of moveDiff) {
+      if (raw + x < 0 || raw + x >= rawSize || col + y < 0 || col + y >= colSize) continue;
+      if (campus[raw + x][col + y] !== "X") {
+        if (campus[raw + x][col + y] === "P") {
+          meetPeopleCnt += 1;
+        }
+        queue.push([raw + x, col + y]);
+        campus[raw + x][col + y] = "X";
+      }
+    }
+  }
+};
+
+bfs(getDoyeonPoint());
+console.log(meetPeopleCnt || "TT");
+
+/* 시간 초과 - 미리 방문 처리 (큐에 중복값 안 들어가도록)
+const bfs = (startPoint) => {
+  const queue = [startPoint];
+  campus[startPoint[0]][startPoint[1]] = "X";
+
+  while (queue.length > 0) {
+    const [raw, col] = queue.shift();
+
+    for (let [x, y] of moveDiff) {
+      if (raw + x < 0 || raw + x >= rawSize || col + y < 0 || col + y >= colSize) continue;
+      if (campus[raw + x][col + y] !== "X") {
+        if (campus[raw + x][col + y] === "P") {
+          meetPeopleCnt += 1;
+        }
+        queue.push([raw + x, col + y]);
+        campus[raw + x][col + y] = "X";
+      }
+    }
+  }
+}; */
+
+/* 정답 - 큐 인덱스로 접근
+const bfs = (startPoint) => {
+  const queue = [startPoint];
 
   let i = 0;
   while (queue.length > i) {
@@ -54,7 +100,30 @@ const bfs = (startPoint) => {
       }
     }
   }
-};
+}; */
 
-bfs(getDoyeonPoint());
-console.log(meetPeopleCnt || "TT");
+/* 시간 초과 - shift 사용
+const bfs = (startPoint) => {
+  const queue = [startPoint];
+
+  while (queue.length > 0) {
+    const [raw, col] = queue.shift();
+
+    if (campus[raw][col] === "P") {
+      meetPeopleCnt += 1;
+    }
+
+    if (campus[raw][col] === "X") {
+      continue;
+    }
+    campus[raw][col] = "X";
+
+    for (let [x, y] of moveDiff) {
+      if (raw + x < 0 || raw + x >= rawSize || col + y < 0 || col + y >= colSize) continue;
+      if (campus[raw + x][col + y] !== "X") {
+        queue.push([raw + x, col + y]);
+      }
+    }
+  }
+};
+ */
