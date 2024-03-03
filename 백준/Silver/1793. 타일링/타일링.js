aacdec32869bab5dcdba98c464d8ccebe3947185
publdaze <78250089+publdaze@users.noreply.github.com>
2024-03-03 22:54:48 +0900

    [Silver II] Title: 타일링, Time: 116 ms, Memory: 9576 KB -BaekjoonHub

---
+++ 백준/Silver/1793. 타일링/타일링.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
+
+const RAW_SIZE = 2;
+const MAX_N = 250;
+const dp = Array.from({ length: RAW_SIZE * MAX_N }, () => Array.from({ length: 1 << RAW_SIZE }, () => -1n));
+const ways = [1];
+
+const go = (idx, status) => {
+  if (idx >= RAW_SIZE * MAX_N) {
+    if (idx === RAW_SIZE * MAX_N && status === 0) return 1n;
+    return 0n;
+  }
+
+  if (dp[idx][status] !== -1n) return dp[idx][status];
+  dp[idx][status] = 0n;
+
+  if (status & (1 << 0)) {
+    dp[idx][status] += go(idx + 1, status >> 1);
+  } else {
+    if (idx % RAW_SIZE < RAW_SIZE - 1 && (status & (1 << 1)) === 0) {
+      if ((status & (1 << 2)) === 0 && (status & (1 << 3)) === 0) {
+        dp[idx][status] += go(idx + 4, status >> 4);
+      }
+      dp[idx][status] += go(idx + 2, status >> 2);
+    }
+    dp[idx][status] += go(idx + 1, (status >> 1) | (1 << (RAW_SIZE - 1)));
+  }
+
+  if (status === 0) {
+    ways.push(dp[idx][status].toString());
+  }
+
+  return dp[idx][status];
+};
+
+go(0, 0);
+console.log(input.map((num) => ways[num]).join("\n"));
