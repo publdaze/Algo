c92da80debfbf6a960bcc784afb014d35734e71b
publdaze <78250089+publdaze@users.noreply.github.com>
2024-03-06 23:07:55 +0900

    [Bronze V] Title: A+B - 2, Time: 128 ms, Memory: 9352 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
+
+const [A, B] = input;
+
+console.log(A + B);
