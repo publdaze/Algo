1ecc3044e4d8b5f3d18fb6a9ad600f9e167c9e90
publdaze <78250089+publdaze@users.noreply.github.com>
2024-04-30 10:00:35 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -1,13 +1,25 @@
-const lcd = (a, b) => {
-    if (a > b) [a, b] = [b, a];
-    for (let i = 1; i <= a * b; i++) {
-        const num = a * i;
-        if (num % b === 0) {
-            return num;
-        }
-    }
+// lcd 풀이 1
+// const lcm = (a, b) => {
+//     if (a > b) [a, b] = [b, a];
+//     for (let i = 1; i <= a * b; i++) {
+//         const num = a * i;
+//         if (num % b === 0) {
+//             return num;
+//         }
+//     }
+// }
+
+function lcm(a, b) {
+  return (a * b) / gcd(a, b);
 }
 
+const gcd = (a, b) => {
+  while (a % b !== 0) {
+    [a, b] = [b, a % b];
+  }
+  return b;
+};
+
 const solution = (arr) => {
-    return arr.reduce((acc, curr) => lcd(acc, curr), 1);
+    return arr.reduce((acc, curr) => lcm(acc, curr), 1);
 }
