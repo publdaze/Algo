1cd06e57b70c37c0c5011d6157a4080118a23c66
publdaze <78250089+publdaze@users.noreply.github.com>
2024-05-28 10:17:06 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 20.66 ms, Memory: 39.2 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -1,3 +1,14 @@
 function solution(files) {
-    return files.map(file => file.split(/(\d+)/)).sort((a, b) => a[0].toUpperCase().localeCompare(b[0].toUpperCase()) || a[1] - b[1]).map(file => file.join(""));
+    return files
+        .map(file => {
+            const [head, number, ...tail] = file.split(/(\d+)/);
+            return { file, head, number };
+        })
+        .sort((a, b) => a.head.toUpperCase().localeCompare(b.head.toUpperCase()) || a.number - b.number)
+        .map(({ file }) => file);
 }
+
+
+// function solution(files) {
+//     return files.map(file => file.split(/(\d+)/)).sort((a, b) => a[0].toUpperCase().localeCompare(b[0].toUpperCase()) || a[1] - b[1]).map(file => file.join(""));
+// }
