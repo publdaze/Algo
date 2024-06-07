0c4049bae52de52064ce7c969ab8c4149c4c1b3e
publdaze <78250089+publdaze@users.noreply.github.com>
2024-06-07 17:00:40 +0900

    [level 2] Title: 호텔 대실, Time: 3.31 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.js
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.js
@@ -1,9 +1,12 @@
-function comvertToMinutes([start, end]) {
+const MAX_MINUTES = 24 * 60 - 1;
+const CLEANUP_TIME = 10;
+
+function convertToMinutes([start, end]) {
     const [startHour, startMinutes] = start.split(":").map(Number);
     const [endHour, endMinutes] = end.split(":").map(Number);
     start = startHour * 60 + startMinutes;
-    end = endHour * 60 + endMinutes + 10;
-    return [start, end > 1440 ? 1440 : end];
+    end = endHour * 60 + endMinutes + CLEANUP_TIME;
+    return [start, end > MAX_MINUTES ? MAX_MINUTES : end];
 }
 
 function numberRangeArr([start, end]) {
@@ -11,12 +14,45 @@ function numberRangeArr([start, end]) {
 }
 
 function solution(book_time) {
-    const times = Array(24 * 60).fill(0);
-    book_time.map(comvertToMinutes).map(numberRangeArr).forEach((range) => {
-        range.forEach((r) => {
-            times[r] += 1;
-        })
-    });
+    const times = Array(MAX_MINUTES + 1).fill(0);
     
-    return Math.max(...times);
+    for (const [start, end] of book_time) {
+        const [startMinute, endMinute] = convertToMinutes([start, end]);
+        times[startMinute]++;
+        times[endMinute]--;
+    }
+    
+    let maxRoom = 0;
+    let currentRoom = 0;
+    for (let i = 0; i <= MAX_MINUTES; i++) {
+        currentRoom += times[i];
+        maxRoom = Math.max(maxRoom, currentRoom);
+    }
+    
+    return maxRoom;
 }
+
+
+
+// function convertToMinutes([start, end]) {
+//     const [startHour, startMinutes] = start.split(":").map(Number);
+//     const [endHour, endMinutes] = end.split(":").map(Number);
+//     start = startHour * 60 + startMinutes;
+//     end = endHour * 60 + endMinutes + 10;
+//     return [start, end > 1440 ? 1440 : end];
+// }
+
+// function numberRangeArr([start, end]) {
+//     return Array.from({ length: end - start }, (_, v) => start + v);
+// }
+
+// function solution(book_time) {
+//     const times = Array(24 * 60).fill(0);
+//     book_time.map(convertToMinutes).map(numberRangeArr).forEach((range) => {
+//         range.forEach((r) => {
+//             times[r] += 1;
+//         })
+//     });
+    
+//     return Math.max(...times);
+// }
