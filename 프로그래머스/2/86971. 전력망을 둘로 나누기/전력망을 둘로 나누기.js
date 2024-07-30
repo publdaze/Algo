6a682024d673ba98ef8a194467314f2f876737ba
publdaze <78250089+publdaze@users.noreply.github.com>
2024-07-30 10:25:20 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 12.68 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -12,45 +12,43 @@
 // 2. 전선 연결이 가장 긴 라인 중 가운데 라인 끊어주기 -> 될 것 같기도..?
 // 4. 라인 하나씩 끊어서 다 측정
 
-function setGraph(graph, arr) {
-    for (let [src, dst] of arr) {
+function setGraph(graph, edges) {
+    for (let [src, dst] of edges) {
         graph[src].push(dst);
         graph[dst].push(src);
     }
 }
 
 function getStartPoint(graph) {
-    return graph.findIndex((src) => src.length > 0);
+    return graph.findIndex((adjList) => adjList.length > 0);
 }
 
 function getTowerCnt(start, graph) {
     const visited = Array(graph.length).fill(false);
     const queue = [start];
     visited[start] = true;
-    
     let cnt = 1;
+    
     while (queue.length) {
         const src = queue.pop();
         
-        if (graph[src]) {
-            for (let dst of graph[src]) {
-                if (visited[dst]) continue;
-                visited[dst] = true;
-                queue.push(dst);
-                cnt++;
-            }
-        }
-        
+        graph[src]?.forEach((dst) => {
+            if (visited[dst]) return;
+            visited[dst] = true;
+            queue.push(dst);
+            cnt++;
+        });
     }
     return cnt;
 }
 
 function solution(n, wires) {
-    return wires.map((line) => {
+    return wires.reduce((minDiff, wire) => {
         const graph = Array.from({ length: n + 1 }, () => []);
-        setGraph(graph, wires.filter((line2) => line !== line2));
-        return graph;
-    }).reduce((acc, graph) => {
-        return Math.min(acc, Math.abs(n - 2 * getTowerCnt(getStartPoint(graph), graph))) 
+        setGraph(graph, wires.filter((w) => w !== wire));
+        const startPoint = getStartPoint(graph);
+        const towerCnt = getTowerCnt(startPoint, graph);
+        const diff = Math.abs(n - 2 * towerCnt);
+        return Math.min(minDiff, diff);
     }, n);
 }
