f43c5718ee21189e6d86bcd4393455f94abb5ba5
publdaze <78250089+publdaze@users.noreply.github.com>
2024-08-06 09:02:34 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.63 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -12,44 +12,76 @@
 // 2. 전선 연결이 가장 긴 라인 중 가운데 라인 끊어주기 -> 될 것 같기도..?
 // 4. 라인 하나씩 끊어서 다 측정
 
+// DP 풀이
 function setGraph(graph, edges) {
-    for (let [src, dst] of edges) {
+    edges.forEach(([src, dst]) => {
         graph[src].push(dst);
         graph[dst].push(src);
-    }
-}
-
-function getStartPoint(graph) {
-    return graph.findIndex((adjList) => adjList.length > 0);
-}
-
-function getTowerCnt(start, graph) {
-    const visited = Array(graph.length).fill(false);
-    const stack = [start];
-    visited[start] = true;
-    let cnt = 1;
-    
-    while (stack.length) {
-        const src = stack.pop();
-        
-        graph[src]?.forEach((dst) => {
-            if (visited[dst]) return;
-            visited[dst] = true;
-            stack.push(dst);
-            cnt++;
-        });
-    }
-    return cnt;
+    });
 }
 
 function solution(n, wires) {
-    return wires.reduce((minDiff, wire) => {
-        const graph = Array.from({ length: n + 1 }, () => []);
-        setGraph(graph, wires.filter((w) => w !== wire));
+    const graph = Array.from({ length: n + 1 }, () => []);
+    setGraph(graph, wires);
+    
+    const parent = Array.from({length: n + 1}, () => -1);
+    const stack = [1];
+    
+    for(let i = 0; i < stack.length; i++) {
+        const parentNode = stack[i];
+        for(const adjacentNode of graph[parentNode]){
+            if(adjacentNode === parent[parentNode]) continue;
+            
+            parent[adjacentNode] = parentNode;
+            stack.push(adjacentNode);
+        }
+    }
+    
+    const towerCnt = Array.from({ length: n + 1 }, () => 1);
+    let minDiff = n;
+    while (stack.length > 0) {
+        const currNode = stack.pop();
+        towerCnt[parent[currNode]] += towerCnt[currNode];
         
-        //const startPoint = getStartPoint(graph);
-        const towerCnt = getTowerCnt(1, graph);
-        const diff = Math.abs(n - 2 * towerCnt);
-        return Math.min(minDiff, diff);
-    }, n);
+        const diff = Math.abs(n - 2 * towerCnt[currNode]);
+        minDiff = Math.min(diff, minDiff);
+    }
+    return minDiff;
 }
+
+// function setGraph(graph, edges) {
+//     for (let [src, dst] of edges) {
+//         graph[src].push(dst);
+//         graph[dst].push(src);
+//     }
+// }
+
+// function getTowerCnt(start, graph) {
+//     const visited = Array(graph.length).fill(false);
+//     const stack = [start];
+//     visited[start] = true;
+//     let cnt = 1;
+    
+//     while (stack.length) {
+//         const src = stack.pop();
+        
+//         graph[src]?.forEach((dst) => {
+//             if (visited[dst]) return;
+//             visited[dst] = true;
+//             stack.push(dst);
+//             cnt++;
+//         });
+//     }
+//     return cnt;
+// }
+
+// function solution(n, wires) {
+//     return wires.reduce((minDiff, wire) => {
+//         const graph = Array.from({ length: n + 1 }, () => []);
+//         setGraph(graph, wires.filter((w) => w !== wire));
+        
+//         const towerCnt = getTowerCnt(1, graph);
+//         const diff = Math.abs(n - 2 * towerCnt);
+//         return Math.min(minDiff, diff);
+//     }, n);
+// }
