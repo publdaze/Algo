d38798c59447300988706f373bb7d660b8f7c99a
publdaze <78250089+publdaze@users.noreply.github.com>
2024-08-28 15:18:23 +0900

    [level 2] Title: 광물 캐기, Time: 0.38 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.js
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.js
@@ -1,28 +1,34 @@
-//REVIEW
+const GROUP_MAX_CNT = 5;
 
-function solution(picks, minerals) {
-    const group = [];
-    while (minerals.length > 0) {
-        const currGroup = minerals.splice(0, 5);
-        group.push(
-            currGroup.reduce((acc, mineral) => {
-                acc[mineral] += 1;
-                return acc;
-            }, { diamond: 0, iron: 0, stone: 0 })
-        );
-    }
-    
-    const picksCnt = picks.reduce((acc, curr) => acc + curr, 0);
-    if (group.length > picksCnt) group.splice(-(group.length - picksCnt));
-    
-    group.sort((a, b) => b.diamond - a.diamond || b.iron - a.iron || b.stone - a.stone);
-    
-    const [diamondPick, ironPick, stonePick] = picks;
-    
-    let minFatigue = 0;
-    minFatigue += group.splice(0, diamondPick).reduce((fatigue, { diamond, iron, stone }) => fatigue + diamond *  1 + iron * 1 + stone * 1, 0);
-    minFatigue += group.splice(0,    ironPick).reduce((fatigue, { diamond, iron, stone }) => fatigue + diamond *  5 + iron * 1 + stone * 1, 0);
-    minFatigue += group.splice(0,   stonePick).reduce((fatigue, { diamond, iron, stone }) => fatigue + diamond * 25 + iron * 5 + stone * 1, 0);
-    return minFatigue;
+function setGroup(minerals) {
+  const groups = [];
+  while (minerals.length > 0) {
+    groups.push(
+      minerals.splice(0, GROUP_MAX_CNT).reduce(
+        (acc, mineral) => {
+          acc[mineral] += 1;
+          return acc;
+        },
+        { diamond: 0, iron: 0, stone: 0 }
+      )
+    );
+  }
+  return groups;
 }
 
+function solution(picks, minerals) {
+  let groups = setGroup(minerals);
+  const [diamondPick, ironPick, stonePick] = picks;
+  groups = groups.slice(0, diamondPick + ironPick + stonePick);
+  groups.sort((a, b) => b.diamond - a.diamond || b.iron - a.iron || b.stone - a.stone);
+
+  let fatigue = 0;
+
+  fatigue += groups.splice(0, diamondPick).reduce((acc, group) => acc + group.diamond + group.iron + group.stone, 0);
+  fatigue += groups.splice(0, ironPick).reduce((acc, group) => acc + group.diamond * 5 + group.iron + group.stone, 0);
+  fatigue += groups
+    .splice(0, stonePick)
+    .reduce((acc, group) => acc + group.diamond * 25 + group.iron * 5 + group.stone, 0);
+
+  return fatigue;
+}
