b527e6fc935954a3650f9ab6b3660892a9d775af
publdaze <78250089+publdaze@users.noreply.github.com>
2024-09-04 14:22:34 +0900

    [level 2] Title: 행렬의 곱셈, Time: 26.84 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -1,16 +1,15 @@
 function solution(arr1, arr2) {
-    const newArr = Array.from({length: arr1.length}, () => Array.from({length: arr2[0].length}, () => 0));
-    
+    const matrix = [];
     for (let i = 0; i < arr1.length; i++) {
-        for (let j = 0; j < arr2.at(0).length; j++) {
-            let sum = 0;
-            for (let k = 0; k < arr2.length; k++) {
-                sum += arr1[i][k] * arr2[k][j];
-                //console.log(i,k,'/', k, j)
+        const row = [];
+        for (let j = 0; j < arr2[0].length; j++) {
+            let cell = 0;
+            for (let k = 0; k < arr1[i].length; k++) {
+                cell += arr1[i][k] * arr2[k][j];
             }
-            newArr[i][j] = sum;
+            row.push(cell);
         }
+        matrix.push(row);
     }
-    
-    return newArr;
+    return matrix;
 }
