7074ee303865a13fd5ac932528c26d564c5c155f
publdaze <78250089+publdaze@users.noreply.github.com>
2024-09-11 16:17:23 +0900

    [level 2] Title: 괄호 회전하기, Time: 27.20 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,38 +1,34 @@
 const PAIR_BRACKET = {
-    "]": "[",
-    "}": "{",
-    ")": "(",
-}
+  "[": "]",
+  "{": "}",
+  "(": ")",
+};
 
 function isValidBrackets(str) {
-    const stack = [];
-    const open = Object.values(PAIR_BRACKET);
+  const stack = [];
 
-    for (let i = 0; i < str.length; i++) {
-        let char = str[i];
-        if (open.includes(char)) {
-            stack.push(char);
-            continue;
-        }
-        if (stack.length === 0) return false;
-        if (stack.at(-1) === PAIR_BRACKET[char]) {
-            stack.pop();
-            continue;
-        }
-
-        return false;
+  for (const char of str) {
+    if (char in PAIR_BRACKET) {
+      stack.push(char);
+      continue;
+    }
+    if (PAIR_BRACKET[stack.pop()] === char) {
+      continue;
     }
 
-    return stack.length === 0;
+    return false;
+  }
+
+  return stack.length === 0;
 }
 
 function solution(s) {
-    let cnt = 0;
+  let cnt = 0;
 
-    for (let i = 0; i < s.length; i++) {
-        const rotatedString = s.slice(i) + s.slice(0, i);
-        if (isValidBrackets(rotatedString)) cnt += 1;
-    }
+  for (let i = 0; i < s.length; i++) {
+    const rotatedString = s.slice(i) + s.slice(0, i);
+    if (isValidBrackets(rotatedString)) cnt += 1;
+  }
 
-    return cnt;
+  return cnt;
 }
