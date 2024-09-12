db18f62f0b71535ce2e01571b8ca5e7ba440d12a
publdaze <78250089+publdaze@users.noreply.github.com>
2024-09-12 14:37:40 +0900

    [level 2] Title: 짝지어 제거하기, Time: 52.06 ms, Memory: 41.9 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -2,17 +2,16 @@ function checkRemovable(s) {
     const stack = [];
     
     for (const char of s) {
-        if (stack.at(-1) !== char) {
+        if (stack[stack.length - 1] !== char) {
             stack.push(char);
             continue;
         }
         stack.pop();
     }
     
-    return !stack.length;
+    return stack.length ? 0 : 1;
 }
 
-
 function solution(s) {
-    return checkRemovable(s) ? 1 : 0;
+    return checkRemovable(s);
 }
