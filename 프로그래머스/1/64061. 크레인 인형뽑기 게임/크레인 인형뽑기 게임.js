a051b3945a150e5eb31fe92eedffd74da8a3a135
publdaze <78250089+publdaze@users.noreply.github.com>
2024-09-24 17:46:35 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.83 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,31 +1,32 @@
+
 function setPositionGroups(board) {
-    const N = board.length;
-    const positionGroups = Array.from({ length: N + 1 }, () => []);
-    
-    for (let i = 0; i < N; i++) {
-        for (let j = 0; j < N; j++) {
-            if (board[j][i] === 0) continue;
-            positionGroups[i + 1].push(board[j][i]);
-        }
-    }
-    
-    positionGroups.forEach((positionGroup) => positionGroup.reverse());
-    
-    return positionGroups;
+  return board[0].map((_, colIndex) =>
+    board
+      .map((row) => row[colIndex])
+      .filter((cell) => cell !== 0)
+      .reverse()
+  );
 }
 
 function solution(board, moves) {
-    const positionGroups = setPositionGroups(board);
-    const stack = [];
-    let bombPair = 0;
-    for (const move of moves) {
-        const currDoll = positionGroups[move].pop();
-        if (stack.length > 0 && currDoll && stack.at(-1) === currDoll) {
-            stack.pop();
-            bombPair += 1;
-            continue;
-        }
-        stack.push(currDoll);
+  const positionGroups = setPositionGroups(board);
+  const bucket = [];
+  let bombCount = 0;
+  for (const move of moves) {
+    const index = move - 1;
+    const currDoll = positionGroups[index].pop();
+
+    if (!currDoll) {
+      continue;
     }
-    return bombPair * 2;
+
+    if (bucket.length > 0 && bucket.at(-1) === currDoll) {
+      bucket.pop();
+      bombCount += 1;
+      continue;
+    }
+
+    bucket.push(currDoll);
+  }
+  return bombCount * 2;
 }
