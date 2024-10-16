488968a0105508ea07ced3a420a611334f112175
publdaze <78250089+publdaze@users.noreply.github.com>
2024-10-17 08:33:02 +0900

    [level 2] Title: 배달, Time: 25.51 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.js
+++ 프로그래머스/2/12978. 배달/배달.js
@@ -7,22 +7,16 @@ function getGraph(N, road) {
     return graph;
 }
 
-function dijkstra(N, graph) {
+function bellmanFord(N, graph) {
     const distances = Array(N + 1).fill(Infinity);
-    const visited = Array(N + 1).fill(false);
-    const pq = [1];
     distances[1] = 0;
-    visited[1] = true;
     
-    while (pq.length > 0) {
-        pq.sort((a, b) => distances[b] - distances[a]);
-        const src = pq.pop();
-        for (const { dst, weight } of graph[src]) {
-            distances[dst] = Math.min(distances[dst], distances[src] + weight);
-            
-            if (visited[dst]) continue;
-            pq.push(dst);
-            visited[dst] = true;
+    for (let i = 1; i < N; i++) {
+        for (let src = 1; src <= N; src++) {
+            for (const { dst, weight } of graph[src]) {
+                if (distances[src] === Infinity) continue;
+                distances[dst] = Math.min(distances[dst], distances[src] + weight);
+            }
         }
     }
     
@@ -31,10 +25,48 @@ function dijkstra(N, graph) {
 
 function solution(N, road, K) {
     const graph = getGraph(N, road);
-    const distances = dijkstra(N, graph)
+    const distances = bellmanFord(N, graph)
     
     return distances.filter((distance) => distance <= K).length;
 }
+
+// function getGraph(N, road) {
+//     const graph = Array.from({ length: N + 1 }, () => []);
+//     for (const [src, dst, weight] of road) {
+//         graph[src].push({ dst, weight });
+//         graph[dst].push({ dst: src, weight });
+//     }
+//     return graph;
+// }
+
+// function dijkstra(N, graph) {
+//     const distances = Array(N + 1).fill(Infinity);
+//     const visited = Array(N + 1).fill(false);
+//     const pq = [1];
+//     distances[1] = 0;
+//     visited[1] = true;
+    
+//     while (pq.length > 0) {
+//         pq.sort((a, b) => distances[b] - distances[a]);
+//         const src = pq.pop();
+//         for (const { dst, weight } of graph[src]) {
+//             distances[dst] = Math.min(distances[dst], distances[src] + weight);
+            
+//             if (visited[dst]) continue;
+//             pq.push(dst);
+//             visited[dst] = true;
+//         }
+//     }
+    
+//     return distances;
+// }
+
+// function solution(N, road, K) {
+//     const graph = getGraph(N, road);
+//     const distances = dijkstra(N, graph)
+    
+//     return distances.filter((distance) => distance <= K).length;
+// }
 
 // dfs -> bfs
 // function getGraph(N, road) {
