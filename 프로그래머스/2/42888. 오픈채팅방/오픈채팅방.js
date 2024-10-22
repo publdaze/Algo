dbc68d8e0b808ad6d49926222cdcdd29856f7c94
publdaze <78250089+publdaze@users.noreply.github.com>
2024-10-22 16:47:50 +0900

    [level 2] Title: 오픈채팅방, Time: 262.47 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -1,17 +1,22 @@
-const logMsg = {
-    Enter: (name) => `${name}님이 들어왔습니다.`,
-    Leave: (name) => `${name}님이 나갔습니다.`,
-}
-
 function solution(record) {
-    const members = record.reduce((acc, log) => {
-        const [, id, name] = log.split(" ");
-        if (name) acc[id] = name;
-        return acc;
-    }, {});
-    
-    return record.map((log) => {
-        const [command, id] = log.split(" ");
-        return logMsg[command]?.(members[id]);
-    }).filter(Boolean);
+  const answer = [];
+  const uid = {};
+
+  for (const line in record) {
+    const cmd = record[line].split(" ");
+    if (cmd[0] != "Leave") {
+      uid[cmd[1]] = cmd[2];
+    }
+  }
+
+  for (const line in record) {
+    const cmd = record[line].split(" ");
+    if (cmd[0] == "Enter") {
+      answer.push(uid[cmd[1]] + "님이 들어왔습니다.");
+    } else if (cmd[0] == "Leave") {
+      answer.push(uid[cmd[1]] + "님이 나갔습니다.");
+    }
+  }
+
+  return answer;
 }
