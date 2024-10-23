db6eeb7b2b973d110f5495d5da12acf6a30f6ac0
publdaze <78250089+publdaze@users.noreply.github.com>
2024-10-23 14:32:23 +0900

    [level 3] Title: 베스트앨범, Time: 0.45 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -1,16 +1,22 @@
+// 노래 재생 많은 장르 -> 많이 재생 노래 -> 고유 번호 낮은 순
+// 장르 별로 가장 많이 재생된 노래를 두 개씩 모아 베스트 앨범
+// 장르별 노래 재생 수 / 
 function solution(genres, plays) {
-    const genreMap = new Map();
-    genres
-      .map((genre, index) => [genre, plays[index]])
-      .forEach(([genre, play], index) => {
-        const data = genreMap.get(genre) || { total: 0, songs: [] };
-        genreMap.set(genre, {
-          total: data.total + play,
-          songs: [...data.songs, { play, index }].sort((a, b) => b.play - a.play).slice(0, 2),
-        });
-      });
-    return [...genreMap.entries()]
-      .sort((a, b) => b[1].total - a[1].total)
-      .flatMap((item) => item[1].songs)
-      .map((song) => song.index);
-  }
+    const genresMap = genres.reduce((map, genre, id) => {
+        if (!map.has(genre)) map.set(genre, { songs: [], totalPlays: 0 });
+        const currGenre = map.get(genre);
+        currGenre.songs.push({ id, play: plays[id] });
+        currGenre.totalPlays += plays[id];
+        return map;
+    }, new Map());
+    
+    
+    return [...genresMap.values()]
+        .sort((a, b) => b.totalPlays - a.totalPlays)
+        .map(({ songs }) => songs
+             .sort((a, b) => b.play - a.play || a.id - b.id)
+             .slice(0, 2)
+             .map(({ id }) => id)
+            )
+        .flat()
+}
