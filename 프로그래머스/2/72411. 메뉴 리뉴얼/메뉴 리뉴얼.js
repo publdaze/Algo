b8f8fd4a0041dd6e6b8810d929b86bd238a5d157
publdaze <78250089+publdaze@users.noreply.github.com>
2024-10-24 15:03:15 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 25.66 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.js
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.js
@@ -1,43 +1,28 @@
-// 가장 많이 주문한, {course 개수}개, 최소 2명 이상 주문, 단품 메뉴 조합 -> 코스메뉴
-// 조합별 주문한 사람 수
-// 코스요리 문자열 오름차순 반환 - 여러 개 가능
-
-function combinations(string, num) {
-    if (num === 0) return [""];
+function combinations (arr, cnt) {
+    if (cnt === 0) return [""];
+    
     const result = [];
-    
-    for (let i = 0; i < string.length; i++) {
-        const fixed = string[i];
-        for (const combination of combinations(string.slice(i + 1), num - 1)) {
-            result.push(fixed + combination);
-        }
-    }
-    
+    arr.forEach((fixed, idx) => {
+        result.push(...combinations(arr.slice(idx + 1), cnt - 1).map((comb) => fixed + comb));
+    })
     return result;
 }
 
 function solution(orders, course) {
-    const menuCombMap = new Map();
+    const result = [];
     
-    for (const order of orders) {
-        for (const num of course) {
-            if (!menuCombMap.has(num)) menuCombMap.set(num, new Map());
-            
-            // course 수 별 나올 수 있는 메뉴 조합
-            const menuCombs = combinations([...order].sort().join(""), num);
-
-            // 조합별 주문한 사람 수 업데이트
-            menuCombs.forEach((menuComb) => {
-                menuCombMap.get(num).set(menuComb, (menuCombMap.get(num).get(menuComb) || 0) + 1);
-            });
+    course.map((cnt) => {
+        const courseMenuCandidate = new Map();
+        
+        for (let order of orders) {
+            for (let combination of combinations([...order], cnt)) {
+                const sortedComb = [...combination].sort().join("");
+                courseMenuCandidate.set(sortedComb, (courseMenuCandidate.get(sortedComb) || 0) + 1)
+            }
         }
-    }
+        const maxMenu = Math.max(...courseMenuCandidate.values());
+        result.push(...[...courseMenuCandidate.entries()].filter(([key, value]) => value === maxMenu && value >= 2).map((item) => item[0]));
+    })
     
-    return [...menuCombMap.values()].flatMap((courseMap) => {
-        if (courseMap.size === 0) return [];
-        const mostOrderCnt = Math.max(...courseMap.values());
-        if (mostOrderCnt < 2) return [];
-        return [...courseMap.entries()].filter(([, value]) => value === mostOrderCnt).map(([key]) => key);
-    }).sort();
-    
+    return result.sort();
 }
