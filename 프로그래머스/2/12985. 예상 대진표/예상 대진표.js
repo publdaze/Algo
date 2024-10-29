298ba45e105f8548d7c70e83f0fd5e2df396ac81
publdaze <78250089+publdaze@users.noreply.github.com>
2024-10-29 14:34:35 +0900

    [level 2] Title: 예상 대진표, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,13 +1,24 @@
+// function solution(n, a, b) {
+    
+//     let round = 0;
+//     while () {
+//         a = Math.ceil(a / 2);
+//         b = Math.ceil(b / 2);
+//         round += 1;
+//     }
+//     return round;
+// }
+
 function solution(n, a, b) {
     if (a > b) [a, b] = [b, a];
     a -= 1;
     b -= 1;
     
-    let round = 1;
-    while (!(a % 2 === 0 && b === a + 1)) {
+    let round = 0;
+    while (a !== b) {
+        round += 1;
         a = Math.floor(a / 2);
         b = Math.floor(b / 2);
-        round += 1;
     }
     return round;
 }
