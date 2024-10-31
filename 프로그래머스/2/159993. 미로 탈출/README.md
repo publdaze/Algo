a4302c7993365b1440ceb32e8665b9f63cc00b62
publdaze <78250089+publdaze@users.noreply.github.com>
2024-10-31 14:57:33 +0900

    [level 2] Title: 미로 탈출, Time: 40.95 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/README.md
+++ 프로그래머스/2/159993. 미로 탈출/README.md
@@ -1,10 +1,10 @@
 # [level 2] 미로 탈출 - 159993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993#) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.43 ms
+메모리: 38.4 MB, 시간: 40.95 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 03일 11:00:20
+2024년 10월 31일 14:57:30
 
 ### 문제 설명
 
--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.js
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.js
@@ -1,43 +1,221 @@
-const DERECTION = [
-    [1, 0],
-    [0, 1],
-    [-1, 0],
-    [0, -1]
-]
+// 벽 못지나감
+// 통로 이동 가능 / 하나의 문 레버로 열 수 있음 (레버 거쳐 문)
+// 한 칸 이동 1초
+
+function findPoint(maps) {
+    const point = {};
+    
+    for (let row = 0; row < maps.length; row++) {
+        for (let col = 0; col < maps[row].length; col++) {
+            if (maps[row][col] === "S") point["S"] = [row, col];
+            else if (maps[row][col] === "L") point["L"] = [row, col];
+            else if (maps[row][col] === "E") point["E"] = [row, col];
+        }
+    }
+    
+    return point;
+}
 
 function outOfRange(row, col, maxRow, maxCol) {
     return row < 0 || col < 0 || row >= maxRow || col >= maxCol;
 }
 
-function bfs(maps, start, endTag) {
-    const [maxRow, maxCol] = [maps.length, maps[0].length];
-    const visited = Array.from({ length: maxRow }, () => Array(maxCol).fill(false));
-    const queue = [start];
-    visited[start[0]][start[1]] = true;
+const DIRECTION = [
+    [1, 0],
+    [-1, 0],
+    [0, 1],
+    [0, -1],
+];
+
+function bfs(start, end, maps) {
+    const [startRow, startCol] = start;
+    const visited = Array.from({ length: maps.length }, () => Array(maps[0].length).fill(false));
+    const queue = [[startRow, startCol, 0]];
+    visited[startRow][startCol] = true;
     
     while (queue.length > 0) {
-        const [row, col, time] = queue.shift();
+        const [row, col, depth] = queue.shift();
         
-        for (let [dRow, dCol] of DERECTION) {
+        for (const [dRow, dCol] of DIRECTION) {
             const [nextRow, nextCol] = [row + dRow, col + dCol];
-            
-            if (outOfRange(nextRow, nextCol, maxRow, maxCol) || maps[nextRow][nextCol] === "X" || visited[nextRow][nextCol]) continue;
-            if (maps[nextRow][nextCol] === endTag) return [nextRow, nextCol, time + 1];
-            queue.push([nextRow, nextCol, time + 1]);
+            if (outOfRange(nextRow, nextCol, maps.length, maps[0].length) || maps[nextRow][nextCol] === "X" || visited[nextRow][nextCol]) continue;
+            if (nextRow === end[0] && nextCol === end[1]) return depth + 1;
+            queue.push([nextRow, nextCol, depth + 1]);
             visited[nextRow][nextCol] = true;
         }
     }
     
-    return [,,null];
+    return -1;
 }
 
 function solution(maps) {
-    for (let row = 0; row < maps.length; row++) {
-        for (let col = 0; col < maps[row].length; col++) {
-            if (maps[row][col] === "S") {
-                const [viaRow, viaCol, time] = bfs(maps, [row, col, 0], "L");
-                return time ? bfs(maps, [viaRow, viaCol, time], "E")?.at(2) || -1 : -1;
-            }
-        }
-    }
+    const { S, L, E } = findPoint(maps);
+    const toL = bfs(S, L, maps);
+    if (toL === -1) return -1;
+    const toE = bfs(L, E, maps);
+    if (toE === -1) return -1;
+    return toL + toE;
 }
+
+// 73.9점 -> col 범위
+// function findPoint(maps) {
+//     const point = {};
+    
+//     for (let i = 0; i < maps.length; i++) {
+//         for (let j = 0; j < maps[i].length; j++) {
+//             if (maps[i][j] === "S") point["S"] = [i, j];
+//             else if (maps[i][j] === "L") point["L"] = [i, j];
+//             else if (maps[i][j] === "E") point["E"] = [i, j];
+//         }
+//     }
+    
+//     return point;
+// }
+
+// function outOfRange(row, col, maxRow, maxCol) {
+//     return row < 0 || col < 0 || row >= maxRow || col >= maxRow;
+// }
+
+// const DIRECTION = [
+//     [1, 0],
+//     [-1, 0],
+//     [0, 1],
+//     [0, -1],
+// ];
+
+// function bfs(start, end, maps) {
+//     const [startRow, startCol] = start;
+//     const visited = Array.from({ length: maps.length }, () => Array(maps[0].length).fill(false));
+//     const queue = [[startRow, startCol, 0]];
+//     visited[startRow][startCol] = true;
+    
+//     while (queue.length > 0) {
+//         const [row, col, depth] = queue.shift();
+        
+//         for (let [dRow, dCol] of DIRECTION) {
+//             const [nextRow, nextCol] = [row + dRow, col + dCol];
+//             if (outOfRange(nextRow, nextCol, maps.length, maps[0].length) || maps[nextRow][nextCol] === "X" || visited[nextRow][nextCol]) continue;
+        
+//             if (nextRow === end[0] && nextCol === end[1]) return depth + 1;
+//             queue.push([nextRow, nextCol, depth + 1]);
+//             visited[nextRow][nextCol] = true;
+//         }
+//     }
+    
+//     return -1;
+// }
+
+// function solution(maps) {
+//     const { S, L, E } = findPoint(maps);
+//     const toL = bfs(S, L, maps);
+//     if (toL === -1) return -1;
+//     const toE = bfs(L, E, maps);
+//     return toL + toE;
+// }
+
+// function findPoint(maps) {
+//     const point = {};
+    
+//     for (let i = 0; i < maps.length; i++) {
+//         for (let j = 0; j < maps[i].length; j++) {
+//             if (maps[i][j] === "S") point["S"] = [i, j];
+//             else if (maps[i][j] === "L") point["L"] = [i, j];
+//             else if (maps[i][j] === "E") point["E"] = [i, j];
+//         }
+//     }
+    
+//     return point;
+// }
+
+// function outOfRange(row, col, maxRow, maxCol) {
+//     return row < 0 || col < 0 || row >= maxRow || col >= maxRow;
+// }
+
+// const DIRECTION = [
+//     [1, 0],
+//     [-1, 0],
+//     [0, 1],
+//     [0, -1],
+// ];
+
+// function bfs(start, end, maps) {
+//     const [startRow, startCol] = start;
+//     const queue = [[startRow, startCol, 0]];
+//     maps[startRow][startCol] = "X";
+    
+//     while (queue.length > 0) {
+//         const [row, col, depth] = queue.shift();
+        
+//         for (let [dRow, dCol] of DIRECTION) {
+//             const [nextRow, nextCol] = [row + dRow, col + dCol];
+//             if (outOfRange(nextRow, nextCol, maps.length, maps[0].length) || maps[nextRow][nextCol] === "X") continue;
+        
+//             console.log(row, col, nextRow, nextCol)
+//             if (nextRow === end[0] && nextCol === end[1]) return depth + 1;
+//             queue.push([nextRow, nextCol, depth + 1]);
+//             maps[nextRow][nextCol] = "X";
+//         }
+//     }
+    
+//     return -1;
+// }
+
+// function solution(maps) {
+//     const { S, L, E } = findPoint(maps);
+//     const toL = bfs(S, L, maps);
+//     if (toL === -1) return -1;
+//     const toE = bfs(L, E, maps);
+//     return toL + toE;
+// }
+
+// 시간 초과
+// function findPoint(maps) {
+//     const point = {};
+    
+//     for (let i = 0; i < maps.length; i++) {
+//         for (let j = 0; j < maps[i].length; j++) {
+//             if (maps[i][j] === "S") point["S"] = [i, j];
+//             else if (maps[i][j] === "L") point["L"] = [i, j];
+//             else if (maps[i][j] === "E") point["E"] = [i, j];
+//         }
+//     }
+    
+//     return point;
+// }
+
+// function outOfRange(row, col, maxRow, maxCol) {
+//     return row < 0 || col < 0 || row >= maxRow || col >= maxRow;
+// }
+
+// const DIRECTION = [
+//     [1, 0],
+//     [-1, 0],
+//     [0, 1],
+//     [0, -1],
+// ];
+
+// function bfs(start, end, maps) {
+//     const queue = [[...start, 0]];
+    
+//     while (queue.length > 0) {
+//         const [row, col, depth] = queue.shift();
+        
+//         for (let [dRow, dCol] of DIRECTION) {
+//             const [nextRow, nextCol] = [row + dRow, col + dCol];
+//             if (outOfRange(nextRow, nextCol, maps.length, maps[0].length) || maps[nextRow][nextCol] === "X") continue;
+//             if (nextRow === end[0] && nextCol === end[1]) return depth + 1;
+//             queue.push([nextRow, nextCol, depth + 1]);
+//             maps[nextRow][nextCol] = "X";
+//         }
+//     }
+    
+//     return -1;
+// }
+
+// function solution(maps) {
+//     const { S, L, E } = findPoint(maps);
+//     const toL = bfs(S, L, maps);
+//     if (toL === -1) return -1;
+//     const toE = bfs(L, E, maps);
+//     return toL + toE;
+// }
