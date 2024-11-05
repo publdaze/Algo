5aec4a5d8bed178011a11900128ca9f5e697ae03
publdaze <78250089+publdaze@users.noreply.github.com>
2024-11-05 16:42:38 +0900

    [level 3] Title: 양과 늑대, Time: 13.36 ms, Memory: 38.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.js
@@ -0,0 +1,35 @@
+function getGraph(n, edges) {
+    const graph = Array.from({ length: n }, () => []);
+    
+    edges.forEach(([src, dst]) => {
+        graph[src].push(dst);
+    });
+    console.log(graph)
+    return graph;
+}
+
+function dfs(graph, info) {
+    const stack = [{ node: 0, sheep: 1, wolf: 0, nextNodes: new Set(graph[0]) }];
+    let maxSheep = 0;
+    
+    while (stack.length > 0) {
+        const { node, sheep, wolf, nextNodes } = stack.pop();
+        
+        maxSheep = Math.max(maxSheep, sheep);
+        
+        for (const nextNode of nextNodes) {
+            if (!info[nextNode] || sheep > wolf + 1) {
+                const newNodes = new Set([...nextNodes, ...graph[nextNode]]);
+                newNodes.delete(nextNode);
+                stack.push({ node: nextNode, sheep: sheep + Number(!info[nextNode]), wolf: wolf + info[nextNode], nextNodes: newNodes });
+            }
+        }
+    }
+    
+    return maxSheep;
+}
+
+function solution(info, edges) {
+    const graph = getGraph(info.length, edges);
+    return dfs(graph, info);
+}
