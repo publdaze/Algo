071c6e7106da184fa49efec5f9775efc6b22b357
publdaze <78250089+publdaze@users.noreply.github.com>
2024-11-07 14:11:26 +0900

    [level 1] Title: 폰켓몬, Time: 22.84 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,5 +1,10 @@
 function solution(nums) {
-    const pokemonTypes = new Set(nums);
-    const need = nums.length / 2;
-    return pokemonTypes.size > need ? need : pokemonTypes.size;
+    const N = nums.length;
+    
+    const type = nums.reduce((acc, num) => {
+        acc.set(num, (acc.get(num) || 0) + 1);
+        return acc;
+    }, new Map());
+    
+    return Math.min(N / 2, type.size);
 }
