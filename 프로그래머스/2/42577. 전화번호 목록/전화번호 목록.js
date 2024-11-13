d485fafbee2af4a4aa296a9aed196fec4fea57c1
publdaze <78250089+publdaze@users.noreply.github.com>
2024-11-13 14:25:08 +0900

    [level 2] Title: 전화번호 목록, Time: 321.25 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,11 +1,35 @@
 function solution(phone_book) {
-    phone_book.sort();
+    const prefix = new Set(phone_book);
     
-    for (let i = 0; i < phone_book.length - 1; i++) {
-        if (phone_book[i + 1].startsWith(phone_book[i])) {
-            return false;
+    for (const phone of phone_book) {
+        for (let i = 1; i < phone.length; i++) {
+            const guessPrefix = phone.slice(0, i);
+            if (prefix.has(guessPrefix)) return false;
         }
     }
-    
     return true;
 }
+
+
+// 3, 4 시간 초과
+// function solution(phone_book) {
+//     return !phone_book.some((phone1) => {
+//         return phone_book.some((phone2) => {
+//             if (phone1 === phone2) return;
+//             return phone2.startsWith(phone1);
+//         });
+//     });
+// }
+
+// 3, 4 시간 초과
+// function solution(phone_book) {
+//     phone_book.sort((a, b) => a.length - b.length);
+    
+//     for (let i = 0; i < phone_book.length; i++) {
+//         for (let j = i + 1; j < phone_book.length; j++) {
+//             if (phone_book[j].startsWith(phone_book[i])) return false;
+//         }
+//     }
+    
+//     return true;
+// }
