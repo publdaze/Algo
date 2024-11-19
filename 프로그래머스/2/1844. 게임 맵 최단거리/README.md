82fc561f7e75bd32444d8f208ecd31e4766f7c8c
publdaze <78250089+publdaze@users.noreply.github.com>
2024-11-19 14:30:10 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 58.03 ms, Memory: 39.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 37.8 MB, 시간: 26.93 ms
+메모리: 39.3 MB, 시간: 58.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 24일 09:44:19
+2024년 11월 19일 14:30:07
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,38 +1,43 @@
 const DIRECTION = [
-    [ 0,  1],
-    [ 0, -1],
-    [ 1,  0],
-    [-1,  0],
-];
+    [1, 0],
+    [0, 1],
+    [-1, 0],
+    [0, -1],
+]
 
-const outOfRange = (row, col, maxRow, maxCol) => {
-    return row < 0 || row >= maxRow || col < 0 || col >= maxCol;
+function outOfRange({ row, col, maxRow, maxCol }) {
+    return row < 0 || col < 0 || row >= maxRow || col >= maxCol;
 }
 
-const bfs = (maps) => {
-    const [maxRow, maxCol] = [maps.length, maps[0].length];
+function isWall(status) {
+    return status === 0;
+}
+
+function bfs(maps, start, end) {
+    const maxRow = maps.length;
+    const maxCol = maps[0].length;
+    
     const visited = Array.from({ length: maxRow }, () => Array(maxCol).fill(false));
-    
-    const queue = [[0, 0, 1]];
     visited[0][0] = true;
+    const queue = [start];
     
-    while (queue.length > 0) {
-        const [row, col, cnt] = queue.shift();
+    let i = 0;
+    while (i < queue.length) {
+        const [r, c, depth] = queue.at(i);
+        if (r === end[0] && c === end[1]) return depth;
         
-        for (const [dRow, dCol] of DIRECTION) {
-            const [nextRow, nextCol] = [row + dRow, col + dCol];
-            
-            if (outOfRange(nextRow, nextCol, maxRow, maxCol) || maps[nextRow][nextCol] === 0 || visited[nextRow][nextCol] === true) continue;
-            if (nextRow === maxRow - 1 && nextCol === maxCol - 1) return cnt + 1;
-            
-            visited[nextRow][nextCol] = true;
-            queue.push([nextRow, nextCol, cnt + 1]);
+        for (const [dr, dc] of DIRECTION) {
+            const [nr, nc] = [r + dr, c + dc];
+            if (outOfRange({ row: nr, col: nc, maxRow, maxCol })|| isWall(maps[nr][nc]) || visited[nr][nc] === true ) continue;
+            queue.push([nr, nc, depth + 1]);
+            visited[nr][nc] = true;
         }
+        i++;
     }
     
     return -1;
 }
 
 function solution(maps) {
-    return bfs(maps);
+    return bfs(maps, [0, 0, 1], [maps.length - 1, maps[0].length - 1]);
 }
