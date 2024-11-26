988477098548c4e78e58940d1b86be0c0e0c4a45
publdaze <78250089+publdaze@users.noreply.github.com>
2024-11-26 14:56:21 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 10.01 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -1,41 +1,41 @@
 function getDividedNetwork(wires, removeWireId) {
-    const newWires = wires.slice();
-    newWires.splice(removeWireId, 1);
-    return newWires;
+  const newWires = wires.slice();
+  newWires.splice(removeWireId, 1);
+  return newWires;
 }
 
 function getGraph(n, linked) {
-    const graph = Array.from({ length: n + 1 }, () => []);
-    linked.forEach(([src, dst]) => {
-        graph[src].push(dst);
-        graph[dst].push(src);
-    });
-    return graph;
+  const graph = Array.from({ length: n + 1 }, () => []);
+  linked.forEach(([src, dst]) => {
+    graph[src].push(dst);
+    graph[dst].push(src);
+  });
+  return graph;
 }
 
 function dfs(n, graph) {
-    const visited = Array.from({ length: n + 1 }, () => false);
-    visited[1] = true;
-    const stack = [1];
-    
-    let cnt = 0;
-    while (stack.length > 0) {
-        const src = stack.pop();
-        cnt++;
-        
-        for (const dst of graph[src]) {
-            if (visited[dst]) continue;
-            stack.push(dst);
-            visited[dst] = true;
-        }
+  const visited = Array.from({ length: n + 1 }, () => false);
+  visited[1] = true;
+  const stack = [1];
+
+  let cnt = 0;
+  while (stack.length > 0) {
+    const src = stack.pop();
+    cnt++;
+
+    for (const dst of graph[src]) {
+      if (visited[dst]) continue;
+      stack.push(dst);
+      visited[dst] = true;
     }
-    return cnt;
+  }
+  return cnt;
 }
 
 function solution(n, wires) {
-    let minDiff = Number.POSITIVE_INFINITY;
-    for (let i = 0; i < n; i++) {
-        minDiff = Math.min(minDiff, Math.abs(n - 2 * dfs(n, getGraph(n, getDividedNetwork(wires, i)))));
-    }
-    return minDiff;
+  let minDiff = Number.POSITIVE_INFINITY;
+  for (let i = 0; i < wires.length; i++) {
+    minDiff = Math.min(minDiff, Math.abs(n - 2 * dfs(n, getGraph(n, getDividedNetwork(wires, i)))));
+  }
+  return minDiff;
 }
