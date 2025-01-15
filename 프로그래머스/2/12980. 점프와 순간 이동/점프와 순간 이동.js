b7c51dfdcf89a2eb073af736e05864f508ac7f0d
publdaze <78250089+publdaze@users.noreply.github.com>
2025-01-15 15:54:39 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.05 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -1,35 +1,37 @@
-// REVIEW
+// K칸 앞으로 점프 - K만큼 사용량
+// or
+// 현재까지 온 거리 * 2로 순간이동 - 사용량 X
+// N까지 이동할 때 최솟
 
-// 이동 - K칸 점프 / (현재까지 온 거리) * 2
-// 건전지 사용량 K칸 점프 시 K 만큼 소요
-// 도착 위치 N
-// 건전지 사용량 최소
+// | 1 | 2 |
+// | 1 | [N-1] + 1 or [N/2] | 
 
-// 다른 사람 풀이 - const solution = (n) => n.toString(2).match(/1/g).length;
-function solution(n) // O(log n)
-{
-    let count = 0;
-
-    while (n > 0) {
-        count += n % 2;
-        n = Math.floor(n / 2);
+function solution(n) {
+    let usage = 0;
+    
+    while (n !== 0) {
+        if (n % 2 === 0) {
+            n /= 2;
+        } else {
+            usage++;
+            n -= 1;
+        }
     }
-
-    return count;
+    
+    return usage;
 }
 
-// 효율성(런타임 에러 + 시간 초과) - O(n) => 배열 크기(118943106 초과 런타임 에러)
-// function solution(n)
-// {
-//     const battery = [0, 1];
+// 메모리 부족, 시간 초과
+// function solution(n) {
+//     const dp = Array.from({ length: n + 1 }, (_, i) => i);
     
 //     for (let i = 2; i <= n; i++) {
-//         if (i % 2 === 0) battery[i] = battery[i / 2];
-//         else battery[i] = battery[Math.floor(i / 2)] + 1;
+//         dp[i] = dp[i - 1] + 1;
+        
+//         if (i % 2 === 0) {
+//             dp[i] = Math.min(dp[i], dp[i / 2]);
+//         }
 //     }
     
-//     return battery.at(-1);
+//     return dp[n];
 // }
-
-
-// 1만큼 이동 후 가능할 때까지 순간이동(X - 1칸보다 많이 이동 후 순간이동이 더 긴거리 순간이동 가능)
