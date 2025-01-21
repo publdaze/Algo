fb25f69341966766fe882bdfa318ddced5798d14
publdaze <78250089+publdaze@users.noreply.github.com>
2025-01-21 15:30:58 +0900

    [level 2] Title: 2 x n 타일링, Time: 6.65 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.js
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.js
@@ -1,11 +1,11 @@
 function solution(n) {
-    const dp = Array(n + 1);
-    dp[0] = 1;
+    const dp = Array.from({ length: n }, () => 0);
     dp[1] = 1;
+    dp[2] = 2;
     
-    for (let i = 2; i < dp.length; i++) {
+    for (let i = 3; i <= n; i++) {
         dp[i] = dp[i - 1] % 1000000007 + dp[i - 2] % 1000000007;
     }
-    
+
     return dp[n] % 1000000007;
 }
