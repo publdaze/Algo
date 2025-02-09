2ec85b05b2207bbe27d9dfc6e9bd1ddab5773f30
publdaze <78250089+publdaze@users.noreply.github.com>
2025-02-09 23:11:25 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 92 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.js
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.js
@@ -1,9 +1,8 @@
-const fs = require("fs");
-const number = Number(
-  fs
-    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
-    .toString()
-    .trim()
-);
+const filePath = process.platform === 'linux' ? 'dev/stdin' : 'BOJ/input.txt';
+const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
 
-console.log("long ".repeat(number / 4) + "int");
+const N = +input[0];
+
+const count = N / 4;
+
+console.log("long ".repeat(count) + "int");
