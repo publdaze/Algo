e31a0196dabebc8608a0837cdfde9fdab4688c45
publdaze <78250089+publdaze@users.noreply.github.com>
2025-02-26 23:21:22 +0900

    [Gold III] Title: 파티, Time: 264 ms, Memory: 15956 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.js
+++ 백준/Gold/1238. 파티/파티.js
@@ -1,3 +1,7 @@
+// - 최단 거리 문제로 다익스트라, 벨만포드 고려
+// - 오고 가는 두 가지 경우에 대해서 처리해야하는데, 다익스트라의 경우 두가지 형태의 그래프 별도로 만들어줘야 해서 벨만포드로 풀기로 함
+// - 벨만포드 알고리즘이 어렴풋이 기억나는데, 정확한 원리가 기억나지 않아서 참고해서 풂
+
 const fs = require("fs");
 const input = fs
   .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
