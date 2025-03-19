1e8b7489208b9478ec4c9dfd2ed946c49198ef85
publdaze <78250089+publdaze@users.noreply.github.com>
2025-03-19 12:55:40 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.75 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,14 +1,25 @@
+// 개인정보 n개
+// 우효기간 전까지 보관, 지나면 파기
+// 모든 달 28일까지로 가정
+// 오늘 날짜로 파기해야 할 개인정보 번호 // 오름차순
+
 function solution(today, terms, privacies) {
-    const termsMap = new Map(terms.map((term) => term.split(" ")));
+    const periodByterm = Object.fromEntries(terms.map((term) => {
+        const [type, period] = term.split(" ");
+        return [type, Number(period)]; // Number를 어느 시점에 적용해야 할 지
+    }));
     
-    return privacies.reduce((accDeletePrivacy, privacy, i) => {
-        const [day, provision] = privacy.split(" ");
-        let [year, month, date] = day.split(".").map(Number);
-        month = month + Number(termsMap.get(provision));
-        year += Math.floor(month / 12);
-        month = month % 12;
-        
-        if (new Date(year, month-1, date) <= new Date(today)) accDeletePrivacy.push(i + 1);
-        return accDeletePrivacy;
-    }, []);
+    const expirationPrivacy = [];
+    for (let [i, privacy] of privacies.entries()) {
+        const [date, term] = privacy.split(" ");
+        const [year, month, day] = date.split(".").map(Number);
+        const expirationDay = day - 1 || 28
+        const expirationMonth = (day - 1 === 0 ? month - 1 : month) + periodByterm[term];
+        const expirationDate = [year + Math.floor(expirationMonth / 12 - (expirationMonth % 12 === 0 ? 1 : 0)), String((expirationMonth % 12) || 12).padStart(2, "0"), String(expirationDay).padStart(2, "0")]
+
+        const tDate = today.split(".").join("");
+        if (Number(expirationDate.join("")) < tDate) expirationPrivacy.push(i + 1);
+    }
+    
+    return expirationPrivacy;
 }
