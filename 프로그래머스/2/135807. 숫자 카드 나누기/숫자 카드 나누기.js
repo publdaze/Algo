4a37a09e5810dd4b82413b9777c7a73648edacfb
publdaze <78250089+publdaze@users.noreply.github.com>
2025-06-18 23:54:56 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 53.69 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.js
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.js
@@ -4,7 +4,7 @@
 // *철수 영희 중복 원소 가능
 // O(n^2) X, O(nlogn) -> 가능한 지 어떻게 파악?
 // 최대공약수 // 여러 숫자들의 최대 공약수를 어떻게 구할 것인가?? // 나온 최대 공약수와의 최대 공약수 구하기
-// 두 조건이 다 만족할 때 둘 중 큰 걸로 해야함!
+// 두 조건이 다 만족할 때 둘 중 큰 걸로 해야함! (조건문 쓸 때 A 기준 먼저 측정해서 두 경우중 더 큰 경우에 대한 판단이 부족한 걸 늦게 인지함)
 
 function gcd(a, b) {
     if (b > a) [a, b] = [b, a];
@@ -15,18 +15,21 @@ function gcd(a, b) {
     return b;
 }
 
-function gcdByArray(arr) {
-    let gcdResult = arr[0];
-    for (const element of arr) {
-        gcdResult = gcd(gcdResult, element);
-    }
-    return gcdResult;
+function gcdOfArray(arr) {
+    return arr.reduce((acc, x) => gcd(acc, x));
 }
 
 function solution(arrayA, arrayB) {
-    const totalGCDA = gcdByArray(arrayA);
-    const totalGCDB = gcdByArray(arrayB);
-    const isValidA = arrayB.every((element) => element % totalGCDA !== 0);
-    const isValidB = arrayA.every((element) => element % totalGCDB !== 0);
-    return isValidA && isValidB ? Math.max(totalGCDA, totalGCDB) : isValidA ? totalGCDA : isValidB ? totalGCDB : 0;
+    const gcdA = gcdOfArray(arrayA);
+    const gcdB = gcdOfArray(arrayB);
+    const someBDivisibleByGcdA = arrayB.some((element) => element % gcdA === 0);
+    const someADivisibleByGcdB = arrayA.some((element) => element % gcdB === 0);
+    
+    
+  if (someBDivisibleByGcdA && someADivisibleByGcdB) {
+    return 0;
+  }
+  if (someBDivisibleByGcdA) return gcdB;
+  if (someADivisibleByGcdB) return gcdA;
+  return Math.max(gcdA, gcdB);
 }
